3976bfd11599ec8a5b8123653fedf123a3b89544
thewhitemagician <aswinamenz@gmail.com>
2024-11-22 21:53:45 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# print('1 for addition')
+# print('2 for substraction')
+# print('3 for multiplication')
+# print('4 for division')
+# choice = int(input('enter your choice:--'))
+# if choice in [1,2,3,4]:
+#     num1 = int(input('enter your number:--'))
+#     num2 = int(input('enter your number:--'))
+
+#     if choice == 1:
+#         result = num1 + num2
+#     elif choice == 2:
+#         result = num1 -  num2
+#     elif choice == 3:
+#         result = num1*num2
+#     elif choice == 4:
+#         result = num1/num2
+
+# print(result)
